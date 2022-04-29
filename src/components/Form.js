c88ed3a4b7492cad6c1c90204fdb0267a1b812e6
unknown <macrowell@yahoo.com>
2022-04-29 07:51:19 -0400

    added name input to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,6 +8,14 @@ const Form = () => {
     return (
         <div id="pizza-form">
             <h2>Build Your Own Pizza</h2>
+            <form>
+                <input 
+                id="name-input"
+                type="text"
+                name="fullName"
+                placeholder="Full Name"
+                />
+            </form>
         </div>
     )
 }
